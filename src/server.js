7913d95276985b3134870b333065ace9bd7d6cfa
T.N De Alwis <45284469+tndealwis@users.noreply.github.com>
2025-11-24 12:24:07 +0530

    small edit

--- src/server.js
+++ src/server.js
@@ -4,9 +4,7 @@ const app = express();
 
 const PORT = process.env.PORT || 8000;
 
-app.get("/", (req, res) => {
-  res.send("this is the root");
-});
+app.get("/", (req, res) => {});
 
 app.listen(PORT, () => {
   console.log(`Server listening at port ${PORT}`);
