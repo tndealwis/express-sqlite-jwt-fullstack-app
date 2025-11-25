7605589464ae795c39e9ef12cbb4989e29f6faa8
TN De Alwis <tharudealwis@gmail.com>
2025-11-25 15:59:58 +0530

    created token

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -10,9 +10,30 @@ router.post("/register", (req, res) => {
   const { username, password } = req.body;
   const hashedPassword = bcrypt.hashSync(password, 8);
 
+  //save the new username hashed password to the database
   try {
+    const insertUser = db.prepare(
+      `INSERT into users (username,password) VALUES(?,?)`
+    );
+    const result = insertUser.run(username, hashedPassword);
+
+    //adding a default todo for the user
+    const defaultTodo = `Add your first todo`;
+    const insertTodo = db.prepare(
+      `INSERT INTO todos (user_id,task) VALUES (?,?)`
+    );
+    insertTodo.run(result.lastInsertRowid, defaultTodo);
+
+    //create a token
+    const token = jwt.sign(
+      { id: result.lastInsertRowid },
+      process.env.SECRET_KEY,
+      { expiresIn: "24h" }
+    );
+
+    res.json({ token });
   } catch (err) {
-    console.loog(err.message);
+    console.log(err.message);
     res.sendStatus(503);
   }
 });
